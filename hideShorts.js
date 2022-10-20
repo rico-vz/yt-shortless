bca1c76c5b64aab7cfc0e2abb95a5454c8aa1aa9
Rico van Zelst <ricoz@tuta.io>
2022-10-20 15:57:23 +0200

    ♥ Now works with all YT Languages

--- hideShorts.js
+++ hideShorts.js
@@ -1,29 +1,77 @@
-var version = "1.0.0";
+// This is a Firefox extension.
+var version = "1.1";
+// 1.1 - 2022/10/20 - Now works with all languages by using path to the "shorts" logo.
+
 console.log("[YT-Shortless] Version " + version + " loaded");
+let hiddenObj = 0;
 
-var richSections = document.querySelectorAll("ytd-rich-section-renderer");
-var guideEntries = document.querySelectorAll("ytd-guide-entry-renderer");
-
-richSections.forEach(function (richSection) {
-  var title = richSection.querySelector(
-    "span#title.style-scope.ytd-rich-shelf-renderer"
+function hideShorts() {
+  var richSections = document.querySelectorAll("ytd-rich-section-renderer");
+  var guideEntries = document.querySelectorAll("ytd-guide-entry-renderer");
+  var miniGuideEntries = document.querySelectorAll(
+    "ytd-mini-guide-entry-renderer"
   );
 
-  if (title.textContent == "Shorts") {
-    richSection.style.display = "none";
+  richSections.forEach(function (richSection) {
+    var title = richSection.querySelector(
+      "span#title.style-scope.ytd-rich-shelf-renderer"
+    );
+
+    if (title.textContent == "Shorts" && richSection.style.display != "none") {
+      richSection.style.display = "none";
+      hiddenObj++;
+    }
+
+    var path = richSection.querySelector("path");
+    if (
+      path.getAttribute("d") ==
+        "M17.77,10.32l-1.2-.5L18,9.06a3.74,3.74,0,0,0-3.5-6.62L6,6.94a3.74,3.74,0,0,0,.23,6.74l1.2.49L6,14.93a3.75,3.75,0,0,0,3.5,6.63l8.5-4.5a3.74,3.74,0,0,0-.23-6.74Z" &&
+      richSection.style.display != "none"
+    ) {
+      richSection.style.display = "none";
+      hiddenObj++;
+    }
+  });
+
+  guideEntries.forEach(function (guideEntry) {
+    var path = guideEntry.querySelector("path");
+
+    if (
+      path != null &&
+      path.getAttribute("d") ==
+        ("M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z" ||
+          "M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z") &&
+      guideEntry.style.display != "none"
+    ) {
+      guideEntry.style.display = "none";
+      hiddenObj++;
+    }
+  });
+
+  miniGuideEntries.forEach(function (miniGuideEntry) {
+    var path = miniGuideEntry.querySelector("path");
+
+    if (
+      path != null &&
+      path.getAttribute("d") ==
+        ("M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z" ||
+          "M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86l-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z") &&
+      miniGuideEntry.style.display != "none"
+    ) {
+      miniGuideEntry.style.display = "none";
+      hiddenObj++;
+    }
+  });
+}
+
+var interval = setInterval(function () {
+  if (hiddenObj >= 3) {
+    console.log(
+      "[YT-Shortless] If shorts are still showing, please create an issue on GitHub: https://github.com/rico-vz/yt-shortless/issues"
+    );
+    clearInterval(interval);
+  } else {
+    hideShorts();
   }
-});
-
-guideEntries.forEach(function (guideEntry) {
-  var title = guideEntry.querySelector("yt-formatted-string");
-
-  if (title.textContent == "Shorts") {
-    guideEntry.style.display = "none";
-  }
-});
-
-console.log("[YT-Shortless] Hid YouTube Shorts");
-console.log(
-  "[YT-Shortless] If shorts are still showing, please create an issue on GitHub: https://github.com/rico-vz/yt-shortless/issues"
-);
+}, 100);
 console.log("[YT-Shortless] Thanks for using YT-Shortless!");
